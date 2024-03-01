f293441423448a14e9449d39e216b1cd493ba379
Erhardt Markus <w.erhardt@bluewin.ch>
2024-03-01 09:59:05 +0100

    ContentLoader

---
+++ Setup.WPF/ViewModels/PageTwoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Setup.WPF.ViewModels
+{
+    public class PageTwoViewModel : Screen
+    {
+
+        protected override async Task OnDeactivateAsync(bool close, CancellationToken cancellationToken)
+        {
+            await _eventAggregator.PublishOnUIThreadAsync("Closing");
+        }
+
+        protected override async Task OnActivateAsync(CancellationToken cancellationToken)
+        {
+            await _eventAggregator.PublishOnUIThreadAsync("Loading");
+            await Task.Delay(2000);
+            await _eventAggregator.PublishOnUIThreadAsync(string.Empty);
+
+        }
+
+
+    }
+}
---
+++ Setup.WPF/Views/PageTwoView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Setup.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for PageTwoView.xaml
+    /// </summary>
+    public partial class PageTwoView : UserControl
+    {
+
+        private IEventAggregator _eventAggregator;
+        public PageTwoView(IEventAggregator eventAggregator)
+        {
+            InitializeComponent();
+            _eventAggregator = eventAggregator;
+        }
+    }
+}
